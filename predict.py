8fe1f38429e122f2ea733e110f38778eecca2f6f
Killian Morin <kmorin@student.42lausanne.ch>
2024-12-02 16:55:08 +0100

    hopefully predict.py is finished, the logic looks good to me | train.py: removed the extraction of data using csv module (DRY, KISS ...), begun the process of training (taken from the ai pro training) but still needs work (a lot)

--- predict.py
+++ predict.py
@@ -1,12 +1,58 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
 from print_color import print
 
-# Mathematical function used to get an estimated price: estimatePrice(mileage) = \theta_0 + (\theta_1 ∗ mileage)
 
 def main():
+    """ Predict the price for a car with a mileage passed as input
+
+    * Prompt the user to put a price to estimate
+    * Try to convert it to an int(), handle the possible exceptions
+    * Read the theta.txt file for the theta values
+    * Catch the possible exceptions (file not created, i.e. training not run) to set the theta at 0
+    * Compute an estimated price with the theta values and the mileage
+        * estimatePrice(mileage) = \theta_0 + (\theta_1 ∗ mileage)
+    * Print the result and create a plot with the dataset, the theta function and the estimation
+    """
     mileage = input("Mileage to get an estimated price for: ")
 
-    print(mileage)
+    try:
+        mileage = int(mileage)
+        print(f"Will estimate the price of a car that has a mileage of {mileage} km\n", tag='information', tag_color='g', color='b')
+    except Exception as e:
+        print(e, color='r')
+        return
+
+    print("Get the theta from theta.txt ...\n", tag='information', tag_color='g', color='w')
+
+    try:
+        with open("theta.txt", "r") as theta_file:
+            print("theta.txt exist, the values of the theta are:", tag='information', tag_color='g', color='cyan')
+            theta0 = float(theta_file.readline()[:-1])
+            theta1 = float(theta_file.readline())
+    except Exception as e:
+        print("theta.txt does not exist, both theta are set to 0", tag='information', tag_color='g', color='cyan')
+        theta0 = 0
+        theta1 = 0
+
+    print(f"theta0 == {theta0}", tag='information', tag_color='g', color='y')
+    print(f"theta1 == {theta1}\n", tag='information', tag_color='g', color='y')
+
+    estimated_price = theta0 + (theta1 * mileage)
+
+    print(f"The estimated price of a car with a mileage of {mileage} is {estimated_price:.4f}", tag='result', tag_color='p', color='b')
+
+    dataset = pd.read_csv("data/data.csv")
+    dataset.plot.scatter(x="km", y="price", c="b", label="Dataset")
+    plt.scatter(x=mileage, y=estimated_price, c="r", label="Estimation")
+    plt.axline(xy1=(0, theta0), slope=theta1, color="g", label="Linear Regression function")
+    plt.title(f"Dataset with the estimated price from the mileage {mileage} with function {theta0} + {theta1}x")
+    plt.xlabel("Mileage (km)")
+    plt.ylabel("Price ($)")
+    plt.legend()
+    plt.grid()
+    plt.savefig(f"estimation_{mileage}.png", dpi=300, bbox_inches='tight')
+
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -1,22 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import csv
-from typing import Tuple
 from print_color import print
 
 
 plt.rcParams["axes.grid"] = True # enable grid by default
 
 
-def data_extraction() -> Tuple[pd.DataFrame, list]:
-    """ Extract data from data.csv using pandas.read_csv() and the python csv module
+def data_extraction() -> pd.DataFrame:
+    """ Extract data from data.csv using pandas.read_csv()
 
     * Use pandas.read_csv() to take the data from the file 'data.csv'
-    * Print the head (five first entries) and the shape of the data
-    * Plot the data in a scatter plot
-    * Use the python csv module to take the data from the file 'data.csv'
-    * Remove the first entry line that corresponds to the labels 'km,price'
     * Print the head (five first entries) and the shape of the data
     * Plot the data in a scatter plot
 
@@ -26,21 +20,21 @@ def data_extraction() -> Tuple[pd.DataFrame, list]:
     Return
     -----
         data_pandas (pandas.DataFrame): the data extracted using pandas
-        data_vanilla (list): the data extracted using csv
     """
 
     print("====================================", color='yellow')
     print("===========DATA EXTRACTION==========", color='yellow')
     print("====================================", color='yellow')
 
-    print("Reading data from data.csv (using pandas.read_csv())", tag='information', tag_color='g', color='w')
+    print("Reading data from data.csv", tag='information', tag_color='g', color='w')
 
     data_frame = pd.read_csv("data/data.csv")
-    print(data_frame[:5], tag='data', tag_color='m', color='w')
+    print(tag='data', tag_color='m', color='w')
+    print(data_frame[:5])
 
-    print("Shape of km:", data_frame["km"].shape, tag='information', tag_color='g', color='w')
-    print("Shape of price:", data_frame["price"].shape, tag='information', tag_color='g', color='w')
-    print("Global shape:", data_frame.shape, tag='information', tag_color='g', color='w')
+    print("Shape of km:", data_frame["km"].shape, tag='data', tag_color='m', color='w')
+    print("Shape of price:", data_frame["price"].shape, tag='data', tag_color='m', color='w')
+    print("Global shape:", data_frame.shape, tag='data', tag_color='m', color='w')
 
     data_frame.plot.scatter(x="km", y="price", c='b')
     plt.title("Dataset: Mileage vs Price (pandas)")
@@ -48,34 +42,9 @@ def data_extraction() -> Tuple[pd.DataFrame, list]:
     plt.ylabel("Price ($)")
     plt.xlim(0, 250000)
     plt.ylim(0, 10000)
-    plt.savefig('dataframe_scatter.png', dpi=300)
+    plt.savefig('dataset_scatter.png', dpi=300)
 
-    print()
-    print("Reading data from data.csv (using python csv module)", tag='information', tag_color='g', color='w')
-
-    with open("data/data.csv", 'r') as data_file:
-        reader = csv.reader(data_file)
-        data_vanilla = list(reader)
-
-    del(data_vanilla[0])
-    data_array = np.array(data_vanilla, dtype='i')
-
-    print(tag='data', tag_color='m')
-    print(data_array[:5], color='w')
-
-    print("Shape of km:", data_array[:, 0].shape, tag='information', tag_color='g', color='w')
-    print("Shape of price:", data_array[:, 1].shape, tag='information', tag_color='g', color='w')
-    print("Global shape:", data_array.shape, tag='information', tag_color='g', color='w')
-
-    plt.scatter(x=data_array[:, 0], y=data_array[:, 1], c='b')
-    plt.title("Dataset: Mileage vs Price (csv module)")
-    plt.xlabel("Mileage (km)")
-    plt.ylabel("Price ($)")
-    plt.xlim(0, 250000)
-    plt.ylim(0, 10000)
-    plt.savefig('list_scatter.png', dpi=300)
-
-    return data_frame, data_array
+    return data_frame
 
 
 def visualize_linear_scatter(theta: np.ndarray, X: pd.Series, y: pd.Series):
@@ -88,33 +57,61 @@ def visualize_linear_scatter(theta: np.ndarray, X: pd.Series, y: pd.Series):
         y (pandas.Series): the price data (what we aim to predict)
     """
 
+    plt.plot(X, y, "ob")
     plt.axline(xy1=(0, theta[0]), slope=theta[1], color='r')
-    plt.scatter(x=X, y=y, color='b')
-    plt.title("Dataset with linear function")
-    plt.savefig('dataset_linear_function.png', dpi=300)
+    plt.title(f"Dataset with linear function at {theta[0]:.2f} + {theta[1]:.2f}")
+    plt.savefig(f'dataset_linear_function_{theta[0]:.2f}_{theta[1]:.2f}.png', dpi=300)
+
+
+def predict(X, theta):
+    predictions = []
+    for i in range(len(X)):
+        pred = X[i] * theta[1] + theta[0]
+        predictions.append(pred)
+    return predictions
+
+
+def fit(X, y, theta, learning_rate, num_iters):
+    # Loop over the number of iterations
+    # Initialize some useful variables
+    m = X.shape[0]
+
+    theta = theta.copy()
+
+    # Loop over the number of iterations
+    for _ in range(num_iters):
+        predictions = predict(X, theta)
+        errors = predictions - y
+
+        theta[0] = theta[0] - (learning_rate / m) * np.sum(errors)
+        theta[1] = theta[1] - (learning_rate / m) * np.sum(errors * X)
+    return theta
 
 
 def main():
 
-    data_frame, data_array = data_extraction()
+    data_frame = data_extraction()
 
     X = data_frame["km"]
     y = data_frame["price"]
 
-    result = X == data_array[:, 0]
-    if result.all() == True:
-        print("X and data_array[:, 0] are equal", tag='information', tag_color='g', color='b')
-    else:
-        print("X and data_array[:, 0] are not equal", tag='information', tag_color='g', color='r')
-    result = y == data_array[:, 1]
-    if result.all() == True:
-        print("y and data_array[:, 1] are equal", tag='information', tag_color='g', color='b')
-    else:
-        print("y and data_array[:, 1] are not equal", tag='information', tag_color='g', color='r')
+    theta = np.random.randn(2)
+    # # theta = np.zeros(2)
+    # print(theta)
 
-    theta = np.zeros(2)
+    # visualize_linear_scatter(theta, X, y)
 
-    visualize_linear_scatter(theta, X, y)
+    # theta = fit(X, y, theta, 0.01, 100)
+    # print(theta)
+
+    # visualize_linear_scatter(theta, X, y)
+
+
+    # print("Save the theta to theta.txt", tag='information', tag_color='g', color='y')
+
+    # with open("theta.txt", "w") as theta_file:
+    #     theta_file.write(str(theta[0]) + "\n")
+    #     theta_file.write(str(theta[1]))
 
 
 if __name__ == "__main__":
